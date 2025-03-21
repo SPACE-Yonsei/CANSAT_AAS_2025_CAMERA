4705bef58c38c81da3208dc76d49ae0cc2ba2ae4
jmpark3972 <75054819+jmpark3972@users.noreply.github.com>
2025-03-22 00:54:46 +0900

    Update csi_camera_0321.py

--- csi_camera_0321.py
+++ csi_camera_0321.py
@@ -1,27 +1,28 @@
 from picamera2 import Picamera2
+from picamera2.encoders import MJPEGEncoder
 import time
+
+
+# zero2에 csi 하나 돌리는 코드입니다
 
 # Picamera2 객체 생성
 picam2 = Picamera2()
 
 # MJPEG 코덱과 640x480 해상도로 영상 녹화용 설정 생성
-video_config = picam2.create_video_configuration({"size": (640, 480), "format": "MJPEG"})
-picam2.configure(video_config)
+# video_config = picam2.create_video_configuration({
+#     "size": (640, 480),
+#     "format": "YUV420"
+# })
+picam2.sensor_mode = 2
+picam2.framerate = 15 / 1
+picam2.resolution = (640, 480)
+# picam2.configure(video_config)
 
 # 카메라 시작 및 안정화 시간 대기
 picam2.start()
 time.sleep(2)  # 카메라가 안정화될 시간을 줍니다.
 
 # 녹화 시작 (파일 확장자는 mjpeg로 저장)
-picam2.start_recording("recorded_video.mjpeg")
+encoder = MJPEGEncoder()
+picam2.start_recording(encoder, "recorded_video.mjpeg")
 print("영상 녹화 시작...")
-
-# 10초간 녹화
-time.sleep(10)
-
-# 녹화 중지
-picam2.stop_recording()
-print("영상 녹화 종료, 파일 저장 완료.")
-
-# 카메라 종료
-picam2.stop()
